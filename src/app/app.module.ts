de414e6edde8eb46b2a3dc3072a22f07bb9a5681
EmmanuelChavezNataren <emmanuel.chavez.condor@gmail.com>
2024-04-05 13:27:00 -0600

    chore: add main header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,17 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '@envs/environment';
+import { MainHeaderComponent } from '@components/headers/main-header/main-header.component';
 import { SideMenuComponent } from '@components/side-menu/side-menu.component';
+import { environment } from '@envs/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-const components = [
-  SideMenuComponent
-]
+const components = [SideMenuComponent, MainHeaderComponent];
 
 @NgModule({
   declarations: [AppComponent],
