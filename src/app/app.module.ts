288894f2891fb6b0f614121642ec948586d5d4ab
EmmanuelChavezNataren <emmanuel.chavez.condor@gmail.com>
2024-04-09 17:26:42 -0600

    chore(deps): add the plugin @angular/cdk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HttpClientModule } from '@angular/common/http';
 import { MainHeaderComponent } from '@components/headers/main-header/main-header.component';
 import { SideMenuComponent } from '@components/side-menu/side-menu.component';
 import { environment } from '@envs/environment';
+import { CoreBoardModule } from '@modules/board/core-board.module';
 import { ThemeEffects } from '@modules/theme/store/theme.effects';
 import { themeReducer } from '@modules/theme/store/theme.reducer';
 import { EffectsModule } from '@ngrx/effects';
@@ -15,7 +17,6 @@ import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoreBoardModule } from '@modules/board/core-board.module';
 
 const components = [SideMenuComponent, MainHeaderComponent];
 
@@ -33,7 +34,8 @@ const components = [SideMenuComponent, MainHeaderComponent];
       maxAge: 25,
       logOnly: environment.production,
     }),
-    CoreBoardModule
+    CoreBoardModule,
+    DragDropModule,
   ],
   providers: [
     {
